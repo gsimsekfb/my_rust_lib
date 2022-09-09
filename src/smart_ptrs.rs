675e3dde66953d6826f27b8c34ab3730beda5ee4
Gokhan Simsek <g.simsek@paralect.com>
2022-09-09 09:36:18 +0300

    Add deref trait examples

--- src/smart_ptrs.rs
+++ src/smart_ptrs.rs
@@ -34,11 +34,52 @@ fn ex1_deref_smart_ptr() {
 }
 
 #[test]
-fn ex2_usage_of_list() {
+fn ex1_usage_of_list() {
   use List::{Cons, Nil};
   let list = Cons(1,
     Box::new(Cons(2,
       Box::new(Cons(3,
         Box::new(Nil))))));
   // assert_eq!(*xx, 42);
+}
+
+// -------------------------------------------------------
+
+// 2. Deref Trait 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+  fn new(x: T) -> MyBox<T> {
+    MyBox(x)
+  }
+}
+
+impl<T> std::ops::Deref for MyBox<T> {
+  type Target = T;
+  fn deref(&self) -> &T {
+    &self.0
+  }
+}
+
+#[test]
+fn ex2_deref() {
+  let x = 5;
+  let y = MyBox::new(x);
+  assert_eq!(5, x);
+  assert_eq!(5, *y); // *y is actually *(y.deref())
+}
+
+fn hello(name: &str) {
+  println!("Hello, {}!", name);
+}
+
+/// Implicit Deref Coercions
+#[test]
+fn ex3_deref_coerc() {
+  let ss = MyBox::new(String::from("aaa"));
+  // Rust turns &MyBox<String> into &String by calling deref. 
+  // Rust calls deref again to turn the &String into &str
+  hello(&ss); // with auto deref coerc.
+    // same as:
+    hello(&(*ss)[..]); // // w/o auto deref coerc.
 }

646432bf681fcec34fce9c0d88c0ccbc130c3f10
Gokhan Simsek <g.simsek@paralect.com>
2022-10-19 09:52:55 +0300

    AAdding Object Safety

--- src/traits.rs
+++ src/traits.rs
@@ -2,8 +2,9 @@ pub trait Draw {
   fn draw(&self);
 }
 
-// Struct that uses trait objects - allows for MULTIPLE
-// concrete types to fill in for the trait object at RUNTIME
+// Struct that uses trait objects - aka DYNAMIC dispatch - 
+// allows for MULTIPLE concrete types to fill in for the trait 
+// object at RUNTIME
 pub struct Screen {
   pub components: Vec<Box<dyn Draw>>,
     // dyn: DYNAMIC dispatch to a trait object    
@@ -16,9 +17,18 @@ impl Screen {
     }
   }
 }
+    // Performance Penalty of Trait Objects (aka Dynamic Dispatch):
+    // When we use trait objects, Rust must use dynamic dispatch. The compiler
+    // doesn’t know all the types that might be used with the code that is using trait
+    // objects, so it doesn’t know which method implemented on which type to call.
+    // Instead, at runtime, Rust uses the pointers inside the trait object to know
+    // which method to call. There is a runtime cost when this lookup happens
+    // that doesn’t occur with static dispatch. Dynamic dispatch also prevents the
+    // compiler from choosing to inline a method’s code, which in turn prevents
+    // some optimizations. However, we did get extra flexibility in the code
 
 // --------------------------------------------------------------------
-// For comparison:
+// For comparison: Static Dispatch
 // Struct that uses a generic type parameter with trait bounds. 
 // A generic type parameter can only be substituted with ONE 
 // concrete type at a time,
@@ -37,6 +47,12 @@ impl<T> Screen_Static_Dispatch<T> where T: Draw {
     }
   }
 }
+    // when we use trait bounds on generics: 
+    // the compiler generates nongeneric implementations
+    // of functions and methods for each concrete type that we use in place
+    // of a generic type parameter. The code that results from monomorphization
+    // is doing static dispatch, which is when the compiler knows what 
+    // method you’re calling at compile time.
 // -------------------------------------------------------------------
 
 
@@ -62,7 +78,6 @@ impl Draw for SelectBox {
 
 #[test]
 fn ex1() {
-
   // Using trait objects to store values of different types that implement
   // the same trait
   let screen = Screen {
@@ -81,4 +96,25 @@ fn ex1() {
   };
   
   screen.run();
+}
+
+// ----------------------------- Object Safety ---------------------------------
+
+// You can only make object-safe traits into trait objects. Some complex rules
+// govern all the properties that make a trait object safe, but in practice, only
+// two rules are relevant. A trait is object safe if all the methods defined in the
+// trait have the following properties:
+//   • The return type isn’t Self.
+//   • There are no generic type parameters.
+// An example of a trait whose methods are not object safe is the standard
+// library’s Clone trait:
+pub struct Screen2 {
+  // pub components: Vec<Box<dyn Clone>>,
+    // Err: Clone` cannot be made into an object
+    // = note: the trait cannot be made into an object because it requires 
+    // `Self: Sized`
+    // = note: for a trait to be "object safe" it needs to allow building
+    // a vtable to allow the call to be resolvable dynamically; for more 
+    // information visit 
+    // <https://doc.rust-lang.org/reference/items/traits.html#object-safety>  
 }

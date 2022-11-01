11784c61ca8615e20a3672050a6d63d82a53b6e5
Gokhan Simsek <g.simsek@paralect.com>
2022-11-01 08:11:16 +0300

    Trait default type parameters

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![allow(non_camel_case_types)]
 
 // mod doc_test; // todo: failed to resolve: use of undeclared crate or module `doc_test`
+mod temp;
 mod mini_grep;
 mod error_handling;
 mod iterators;
@@ -16,3 +17,4 @@ mod averaged_collection;
 mod traits;
 mod trait_object_vs_struct_obj;
 mod traits_associated_type_vs_generics;
+mod trait_default_type_parameter;
---
+++ src/trait_default_type_parameter.rs
@@ -0,0 +1,48 @@
+use std::ops::Add;
+
+// Original Add trait
+pub trait Add_<Rhs = Self> { // <Rhs = Self>: default type parameter
+    type Output;
+    fn add(self, rhs: Rhs) -> Self::Output;
+}
+
+// 1.a. Using "default type parameter"
+// Implementing the Add trait to overload the + operator for Point instances
+#[derive(Debug, PartialEq)]
+struct Point { x: i32, y: i32 }
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[test] fn ex1_a() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+}
+
+// 1.b. Overriding "default type parameter"
+#[derive(Debug, PartialEq)] struct Millimeters(u32);
+#[derive(Debug, PartialEq)] struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+#[test] fn ex1_b() {
+  assert_eq!(
+      Millimeters(3) + Meters(1),
+      Millimeters(1003)
+  );
+}

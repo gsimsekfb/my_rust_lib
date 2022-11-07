a0d7a8b90ff06da5a85219e11789d5a156a25fea
Gokhan Simsek <g.simsek@paralect.com>
2022-11-07 17:36:11 +0300

    Minor change

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -29,3 +29,17 @@ fn ex_3() {
 }
 
 //// ---------
+ 
+//// Rust groups errors into two major categories: recoverable and unrecoverable 
+//// errors. For a recoverable error, such as a file not found error, 
+//// we most likely just want to report the problem to the user and retry the 
+//// operation. Unrecoverable errors are always symptoms of bugs, like trying 
+//// to access a location beyond the end of an array, and so we want to 
+//// immediately stop the program.
+//// Src: https://doc.rust-lang.org/book/ch09-00-error-handling.html
+
+//// gs2022: 
+//// So, in summary, ideally (in production code), we want to recover/handle 
+//// or return that error/result to caller (and not use panic or exit or expect 
+//// or unwrap at all - they are all the same in the sense they stop the program)
+//// for recoverable errors.

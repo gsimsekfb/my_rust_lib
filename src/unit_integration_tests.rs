d39d58322ea40e3918ab9a34bc4b486ed6c19725
Gökhan Şimşek <g.simsek@paralect.com>
2023-10-16 14:44:17 +0300

    Update unit_integration_tests.rs

--- src/unit_integration_tests.rs
+++ src/unit_integration_tests.rs
@@ -1,3 +1,10 @@
+// integration tests:
+// In Rust, integration tests are entirely external to your library. 
+// They use your library in the same way any other code would, which 
+// means they can only call functions that are part of your library’s public API. 
+// Their purpose is to test whether many parts of your library work together correctly.
+// https://doc.rust-lang.org/book/ch11-03-test-organization.html#integration-tests
+
 #[test]
 fn test_add_one() {
     assert_eq!(2, crate::unit::add_one(1));

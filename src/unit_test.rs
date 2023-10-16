19d2062845edca6393d53674b529c22b5727a5a8
Gökhan Şimşek <g.simsek@paralect.com>
2023-10-16 14:45:53 +0300

    Update and rename unit.rs to unit_test.rs

--- src/unit_test.rs
+++ src/unit_test.rs
@@ -1,4 +1,10 @@
 
+// Unit Tests
+// The purpose of unit tests is to test each unit of code in isolation from 
+// the rest of the code to quickly pinpoint where code is and isn’t working 
+// as expected. You’ll put unit tests in the src directory in each file with 
+// the code that they’re testing. 
+// Src: 
 // https://doc.rust-lang.org/book/ch11-03-test-organization.html#unit-tests
     
 fn add_const() -> i32 {

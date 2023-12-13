55b0ae7704ab1094769033625774e0f907e0c668
Gökhan Şimşek <g.simsek@paralect.com>
2023-12-13 11:27:37 +0300

    Minor change

--- src/let_else.rs
+++ src/let_else.rs
@@ -1,21 +1,27 @@
 use std::str::FromStr;
 
-fn get_number(s: &str) -> u64 {
+fn process_str(s: &str) -> u64 {
     // Using it like an "early return"
     let Ok(count) = u64::from_str(s) else {
         panic!("Can't parse integer: '{s}'");
     };
-    // ...
+    // instead of
+    let res = u64::from_str(s);
+    if res.is_err() {
+        return 42
+    }
+    let count = res.unwrap();
+    // ... do_something_with(count)
     count
 }
 
 #[test] 
 fn ex1_simple_works() {
-    assert_eq!(get_number("3"), 3);
+    assert_eq!(process_str("3"), 3);
 }
 
 #[test]
 #[should_panic]
 fn ex2_panics() {
-    assert_eq!(get_number("aa"), 3);
+    assert_eq!(process_str("aa"), 3);
 }

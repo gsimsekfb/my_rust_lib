b3a583a3db4a489c61bfbdf9e0749905fdad5e89
Gökhan Şimşek <g.simsek@paralect.com>
2024-01-05 11:43:34 +0300

    Minor change

--- src/traits_associated_constants.rs
+++ src/traits_associated_constants.rs
@@ -1,7 +1,8 @@
 // https://doc.rust-lang.org/reference/items/associated-items.html#associated-constants
 
 trait MyTrait {
-    const MY_CONST: u8; // Associated Constant
+    const MY_CONST: u8;                 // Associated Constant
+    const CONST_WITH_DEFAULT: i32 = 99; // Associated Constant w/ default value
     fn bar();
 }
 
@@ -15,4 +16,5 @@ impl MyTrait for Foo {
 #[test]
 fn ex1() {
     assert_eq!(Foo::MY_CONST, 42);
+    assert_eq!(Foo::CONST_WITH_DEFAULT, 99);
 }

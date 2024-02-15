99ecf182c72d3cb3102e329b50a0472b2ec01fb7
Gökhan Şimşek <g.simsek@paralect.com>
2024-02-15 13:36:10 +0300

    Simple benchmark example

---
+++ examples/bench.rs
@@ -0,0 +1,21 @@
+/* 
+Run: 
+    cargo r --release --example bench
+Also see benches/my_benchmark.rs 
+*/
+
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n-1) + fibonacci(n-2),
+    }
+}
+
+fn main() {
+    let now = std::time::Instant::now();
+    fibonacci(20);
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);  
+}
+//  Elapsed: 27.24µs

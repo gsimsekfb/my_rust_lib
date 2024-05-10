554b4da2f19cc59308226d78197de0ff1aeb01ff
Gökhan Şimşek <g.simsek@paralect.com>
2024-05-10 13:50:15 +0300

    Refactoring

--- examples/maze-1d-with-cargo-toml/main.rs
+++ examples/maze-1d-with-cargo-toml/main.rs
@@ -4,5 +4,5 @@ use my_rust_lib::maze_1d::*;
 fn main() {
     let start_pos = 2;
     let start_dir = Dir::L;
-    goto(&start_dir, start_pos, &mut steps, &mut all_steps);
+    start_game(&start_dir, start_pos);
 }
--- src/maze_1d.rs
+++ src/maze_1d.rs
@@ -3,8 +3,12 @@
 pub enum Dir { L, R }  // Left, Right
 
 // 1D world
-// 1 is open, 0 is closed box
-const W: [usize; 5] = [1, 0, 0, 1, 1];
+// 1 open, 0 closed, cell [0,1,2,3,4,5,6] 
+const WORLD: [usize; 5] = [1,0,0,1,1];
+// const WORLD: [usize; 7] = [0,1,1,0,1,1,0]; // todo
+
+// todo: 
+// - save R dir is closed on failure
 
 pub fn start_game(
     dir: &Dir,
@@ -21,10 +25,14 @@ pub fn start_game(
     println!("all_steps: {:?}\n", all_steps);
 }
 
+pub fn got_stuck(pos: usize) {
+    println!("Got stuck! All cells around are closed. pos: {pos}");
+    std::process::exit(0);
+}
+
 pub fn print_info(start_dir: &Dir, start_pos: usize) {
-    println!("");
-    println!("--INFO-------------------------");
-    println!("World: {:?}", W);
+    println!("\n--INFO-------------------------");
+    println!("World: {:?}", WORLD);
     println!("Start pos: {start_pos}");
     println!("Start dir: {:?}", start_dir);
     println!("starting game...");
@@ -39,34 +47,33 @@ pub fn goto(
 ) {
     // println!("-- going from {pos}");
     if pos == 0 { return }
-    if pos == W.len()-1 { return }
+    if pos == WORLD.len()-1 { return }
+    if all_steps.len() == 10 { 
+        println!("\n!!! Error: # of steps limit exceeded"); return;
+    }
     match dir {
         Dir::L => {
             all_steps.push(dir.clone());
-            if W[pos-1] == 1 {  // Open cell
-                // println!("L");
+            if WORLD[pos-1] == 1 {  // Open cell
+                println!("L");
                 steps.push(dir.clone());
                 goto(dir, pos-1, steps, all_steps);
+            } else if WORLD[pos+1] == 1 { // Open cell
+                goto(&Dir::R, pos, steps, all_steps);
             } else {
-                if W[pos+1] == 0 { // Closed cell
-                    panic!("All ways are closed. pos: {pos}");
-                } else {
-                    goto(&Dir::R, pos, steps, all_steps);
-                }
+                got_stuck(pos);
             }
         }
         Dir::R => {
             all_steps.push(dir.clone());
-            if W[pos+1] == 1 {
-                // println!("R");
+            if WORLD[pos+1] == 1 {
+                println!("R");
                 steps.push(dir.clone());
                 goto(dir, pos+1, steps, all_steps)
-            } else {
-                if W[pos-1] == 0 {
-                    panic!("All ways are closed. pos: {pos}");
-                } else {                
-                    goto(&Dir::L, pos, steps, all_steps);
-                }
+            } else if WORLD[pos-1] == 1 {
+                goto(&Dir::L, pos, steps, all_steps);
+            } else {                
+                got_stuck(pos);
             }
         }
     };

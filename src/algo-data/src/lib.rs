641205ae4ebee586ae1985e10435e071620ce166
Gokhan Simsek <gokhansimsekfb@gmail.com>
2024-08-27 14:46:25 +0300

    Adding tree_serial_deserialize.rs and string_len_longest_substr_w_nonrepeat_chars.rs

--- src/algo-data/src/lib.rs
+++ src/algo-data/src/lib.rs
@@ -4,12 +4,12 @@
 #![allow(non_camel_case_types)]
 #![allow(clippy::four_forward_slashes)]
 
-mod tree;
-mod tree_1_max_height;
-mod tree_print;
 
 mod array_two_sum;
-
 mod bit_count_set_bits;
-
 mod matrix_set_bits_at_row_column;
+mod string_len_longest_substr_w_nonrepeat_chars;
+mod tree;
+mod tree_1_max_height;
+mod tree_serial_deserialize;
+mod tree_print;
---
+++ src/algo-data/src/tree_serial_deserialize.rs
@@ -0,0 +1,188 @@
+// Problem Description
+// Serialize and Deserialize a Binary Tree
+// e.g.
+//        1 
+//   21      
+//     31
+// 
+// csv: 1 21 0 31
+//
+// https://www.geeksforgeeks.org/serialize-deserialize-binary-tree/
+
+#[derive(Debug, PartialEq)]
+struct N {
+    v: i32,
+    l: Option<Box<N>>,
+    r: Option<Box<N>>
+}
+
+fn new_leaf(v: i32) -> Option<Box<N>> {
+    Some(Box::new( N { v, l: None, r: None} ))
+}
+
+fn new_node(v: i32, l: Option<Box<N>>, r: Option<Box<N>>) -> Option<Box<N>> {
+    Some(Box::new( N { v, l, r} ))
+}
+
+// Sol 1 
+// Hint: ser fn is easier, start with it
+// Hint: Use recursive fn, do sth w/ current node or its value then
+// traverse/recurse on lefts and then rights. 
+
+//        1 
+//   21      
+//     31
+// 
+// csv: 1 21 0 31
+fn ser(n: &Option<Box<N>>, res: &mut Vec<i32> ) {
+    if n.is_none() { res.push(0);   /*print!("0-");*/    return; }
+    let v = n.as_ref().unwrap().v;
+    res.push(v);    /*print!("{v}-");*/ 
+    ser(&n.as_ref().unwrap().l, res);
+    ser(&n.as_ref().unwrap().r, res);
+}
+// Debug
+// f( 1, l: (21, l:null, r: 31), null )
+// v = 1
+// f( 21, l:null, r:31 )
+//   v = 21
+//   f ( null ) ret
+//   f ( 31 )
+//     v = 31
+//     f( null ) ret
+//     f( null ) ret
+//   ret
+// f ( null ) ret
+// end
+
+//        1 
+//   21      
+//     31
+// 
+// csv: 1 21 0 31
+fn des(iter: &mut core::slice::Iter<i32>) -> Option<Box<N>> {
+    if let Some(e) = iter.next() {
+        // print!("{e}-");
+        if *e == 0 { return None };
+        let mut n = new_leaf(*e);
+        n.as_deref_mut().unwrap().l = des(iter);
+        n.as_deref_mut().unwrap().r = des(iter);
+        return n;
+    }
+    None
+}
+// Debug
+// f(1)
+// n.v = 1
+// n.l = f(21)
+//   f(21) 
+//   n.v = 21
+//   n.l = f(0) ret None
+//   n.r = f(31)
+//     f(31)
+//     n.l = None
+//     n.r = None 
+//     ret 31, None, None
+//   ret 21, None, (31, None, None)
+// n.r = None
+// ret v:1, l: (v:21, l:None, r:(v:31, l:None, r:None)), r:None 
+
+
+
+#[test] fn tt() {
+
+    // Sol-1
+
+    // --------- Deserialize 
+
+    //        1 
+    //   21      
+    //     31
+    // 
+    // csv: 1 21 0 31
+    let csv = [1, 21, 0, 31];
+    let tree = des(&mut csv.iter());
+    let res = new_node(
+        1,
+        new_node(21, None, new_leaf(31)),
+        None
+    );
+    assert_eq!(tree, res);
+
+    //        1 
+    //  21        22
+    //     31
+    // 
+    // csv: 1 21 0 31 0 0 22
+    let csv = [1,21,0,31,0,0,22];
+    let tree = des(&mut csv.iter());
+    let res = new_node(
+        1, 
+        new_node(21, None, new_leaf(31)), 
+        new_leaf(22)
+    );
+    assert_eq!(tree, res);
+
+    //                  1
+    //       21                   22
+    //   333     334
+    //               4444
+    //
+    // csv: 1 21 333 0 0 334 0 4444 0 0 22
+    let csv = [1,21,333,0,0,334,0,4444,0,0,22];
+    let tree = des(&mut csv.iter());
+    let res = new_node(
+        1, 
+        new_node(21, new_leaf(333), new_node(334, None, new_leaf(4444))), 
+        new_leaf(22)
+    );
+    assert_eq!(tree, res);
+
+
+    // ------- Serialize 
+
+    //        1 
+    //   21      
+    //     31
+    //
+    // csv: 1 21 0 31
+    let tree = new_node(
+        1,
+        new_node(21, None, new_leaf(31)),
+        None
+    );
+    let mut res = vec![];
+    ser(&tree, &mut res);
+    assert_eq!(res, [1,21,0,31,0,0,0]);
+
+
+    //        1 
+    //  21        22
+    //     31
+    // 
+    // csv: 1 21 0 31 0 0 22
+    let tree = new_node(
+        1, 
+        new_node(21, None, new_leaf(31)), 
+        new_leaf(22)
+    );
+    let mut res = vec![];
+    ser(&tree, &mut res);
+    assert_eq!(res, [1,21,0,31,0,0,22,0,0]);
+
+    //                  1
+    //       21                   22
+    //   333     334
+    //               4444
+    //
+    // csv: 1 21 333 0 0 334 0 4444 0 0 22
+    let tree = new_node(
+        1, 
+        new_node(21, new_leaf(333), new_node(334, None, new_leaf(4444))), 
+        new_leaf(22)
+    );
+    let mut res = vec![];
+    ser(&tree, &mut res);
+    assert_eq!(res, [1,21,333,0,0,334,0,4444,0,0,22,0,0]);
+
+}

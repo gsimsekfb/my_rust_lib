7105f0298fc8e4e60afb06d1e639fac9b7fe2bd3
Gökhan Simsek <gokhansimsekfb@gmail.com>
2025-05-27 11:13:11 +0300

    refactoring

--- src/algo-data/src/array_two_sum.rs
+++ src/algo-data/src/array_two_sum.rs
@@ -1,28 +1,28 @@
 
 // Problem Description
 // Given an array of integers and an integer sum, 
-// return the two numbers (or indices) that add up to sum.
+// return the indices that add up to sum.
 //
-// e.g. let arr = [5,6,7,8], sum=13, return (6,7)
+// e.g. let arr = [5,6,7,8], sum=13, return (0,3) and (1,2)
 //
 // https://www.geeksforgeeks.org/check-if-pair-with-given-sum-exists-in-array/
 // https://medium.com/@AlexanderObregon/solving-the-two-sum-problem-on-leetcode-rust-solutions-walkthrough-26a2e8b48616
 
-#[test] fn all_solutions() {    
+#[test]
+fn all_solutions() {    
     // Sol-1
     let arr = [1,2,3,4];
-    assert_eq!(two_sum(&arr, 0), None);
-    assert_eq!(two_sum(&arr, 7), Some((3,4)));
+    assert_eq!(two_sum(&arr, 5), &[(0,3), (1,2)]);
 
     // Sol-2
     let arr = [5,6,7,8];
-    assert_eq!(two_sum_2(&arr, 14), Some((6,8)));
+    assert_eq!(two_sum_2(&arr, 14), &[(1,3)]);
 
     // Sol-3
     let arr = [5,6,7,8];
-    assert_eq!(two_sum_3(&arr, 14), Some((8,6)));
+    assert_eq!(two_sum_3(&arr, 13), &[(0,3), (1,2)]);
     let arr = [5,6,7,8,9,10];
-    assert_eq!(two_sum_3(&arr, 17), Some((9,8)));
+    assert_eq!(two_sum_3(&arr, 17), &[(2,5), (3,4)]);
 }
 
 // Solution 1: Brute Force Approach
@@ -34,18 +34,19 @@
 // Time Complexity: O(n*n) (approximately n*(n-1)/2 comparisons)
 // Space Complexity: O(1), no additional data structures and
 // the space required does not depend on the size of the input array.
-// e.g. let arr = [1,2,3,4]
-fn two_sum(arr: &[i32], sum: i32) -> Option<(i32, i32)> {
-    for i in 0..arr.len()-1 { // i: 0 .. 2
-        // println!("{i}");
-        for j in i+1..arr.len() {  // j: i+1 .. 3
-            // println!("{i} {j}");
-            if arr[i] + arr[j] == sum { return Some((arr[i], arr[j])) }
+fn two_sum(arr: &[i32], sum: i32) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+
+    for i in 0..arr.len() {
+        for j in i+1..arr.len() {
+            // println!("{},{}", arr[i], arr[j]);
+            if arr[j] + arr[i] == sum {
+                res.push((i,j));
+            }
         }
     }
-    None
+    res
 }
-
 
 // Solution 2: Two-Pass Hash Table
 // Using a hash map to improve time complexity. 
@@ -56,18 +57,31 @@ fn two_sum(arr: &[i32], sum: i32) -> Option<(i32, i32)> {
 // operations take constant time, leading to linear time complexity.
 // Space Complexity: O(n) — The hash map stores up to ’n’ elements, 
 // resulting in linear space complexity.
-// e.g. let arr = [5,6,7,8], sum=13, return (6,7)
-fn two_sum_2(arr: &[i32], sum: i32) -> Option<(i32, i32)> {
-    let mut map = std::collections::HashMap::new();
+fn two_sum_2(arr: &[i32], sum: i32) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+    use std::collections::HashMap;
+    let mut map: HashMap<&i32,usize> = HashMap::new();
+    // map: 5,0 - 6,1 - 7,2 - 8,3
     for (i, e) in arr.iter().enumerate() {
         map.insert(e, i);
-    } // map {8: 3, 7: 2, 6: 1, 5: 0}
-    for (i, e) in arr.iter().enumerate() {
-        if let Some(index) = map.get(&(sum-e)) {
-            return Some((arr[i], arr[*index]))
-        }
     }
-    None
+    for (i, e) in arr.iter().enumerate() {
+        if let Some(val) = map.get(&(sum-e)).filter(|val| **val > i) {
+            res.push((i, *val));
+        }
+        // or
+        //
+        // // same as above w/o filter
+        // if let Some(val) = map.get(&(sum-e)) {
+        //     if *val > i { res.push((i, *val)); }
+        // }
+        //
+        // // w/o if let
+        // if map.contains_key(&(sum-e)) && map[&(sum-e)] > i {            
+        //     res.push((i, map[&(sum-e)]));
+        // }       
+    }
+    res
 }
 
 
@@ -80,14 +94,16 @@ fn two_sum_2(arr: &[i32], sum: i32) -> Option<(i32, i32)> {
 // operations take constant time, leading to linear time complexity.
 // Space Complexity: O(n) — The hash map stores up to ’n’ elements, resulting 
 // in linear space complexity.
-// e.g. let arr = [5,6,7,8], sum=14, return (6,8)
-fn two_sum_3(arr: &[i32], sum: i32) -> Option<(i32, i32)> {
-    let mut map = std::collections::HashMap::new();
+fn two_sum_3(arr: &[i32], sum: i32) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+    use std::collections::HashMap;
+    let mut map: HashMap<&i32,usize> = HashMap::new();
+    // map: 5,0 - 6,1 ... 8,3
     for (i, e) in arr.iter().enumerate() {
-        if let Some(index) = map.get(&(sum-e)) {
-            return Some((arr[i], arr[*index]))
-        }
         map.insert(e, i);
-    } // map {8: 3, 7: 2, 6: 1, 5: 0}
-    None
+        if let Some(val) = map.get(&(sum-e)) {
+            res.push((*val, i));
+        }        
+    }
+    res.into_iter().rev().collect()
 }
